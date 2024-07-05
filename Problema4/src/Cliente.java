8b8aa84ccc72a4875638bc5bd74fce05a6256d6f
Pedro-Case <137705805+Pedro-Case@users.noreply.github.com>
2024-07-05 20:09:25 -0300

    Problema 4

---
+++ Problema4/src/Cliente.java
@@ -0,0 +1,25 @@
+public class Cliente extends Thread{
+    int nome;
+    Assentos assento;
+
+    public Cliente(int nome, Assentos assento) {
+        this.nome = nome;
+        this.assento = assento;
+    }
+
+    public void run(){
+        try {
+            System.out.println("Cliente " + nome + " entrou no restaurante");
+            assento.sentar(this);
+            sleep(500);
+            assento.atualizando_mesa.lock();
+            assento.comeram++;
+            System.out.println(nome + " terminou de comer " + assento.comeram);
+            assento.atualizando_mesa.unlock();
+
+        }
+        catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
