333c131d23be580eebabbfec64fa312410a393ba
Pedro-Case <137705805+Pedro-Case@users.noreply.github.com>
2024-07-05 22:44:34 -0300

    Corrigindo erros de conflito

--- Problema4/src/Cliente.java
+++ Problema4/src/Cliente.java
@@ -1,30 +1,3 @@
-<<<<<<< Updated upstream
-public class Cliente extends Thread{
-    int nome;
-    Assentos assento;
-
-    public Cliente(int nome, Assentos assento) {
-        this.nome = nome;
-        this.assento = assento;
-    }
-
-    public void run(){
-        try {
-            System.out.println("Cliente " + nome + " entrou no restaurante");
-            assento.sentar(this);
-            sleep(500);
-            assento.atualizando_mesa.lock();
-            assento.comeram++;
-            assento.sentar_assentos();
-            System.out.println(nome + " terminou de comer " + assento.comeram);
-            assento.atualizando_mesa.unlock();
-
-        }
-        catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-=======
 public class Cliente extends Thread{
     int nome;
     Mesa mesa;
@@ -49,5 +22,4 @@ public class Cliente extends Thread{
             throw new RuntimeException(e);
         }
     }
->>>>>>> Stashed changes
 }
